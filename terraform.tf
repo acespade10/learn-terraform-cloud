e6cfa1b2d1dc7be3b0a3f05024ed6cd8be9e4f0f
acespade10 <hidanieltj@gmail.com>
2024-02-12 23:48:04 -0800

    Remove cloud block

--- terraform.tf
+++ terraform.tf
@@ -1,12 +1,13 @@
 terraform {
-
+/*
   cloud {
-    organization = "organization-name"
+    organization = "bigLab"
 
     workspaces {
       name = "learn-terraform-cloud"
     }
   }
+*/
 
   required_providers {
     aws = {
